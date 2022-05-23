61f00ee7daeff8473ae0bb85409203d571413177
commit <>
2022-05-23 15:06:55 +0300

    pr changes

--- server-side/api.ts
+++ server-side/api.ts
@@ -1,9 +1,11 @@
-import UtilitiesService from './utilities.service'
+import AddonsService from './addons.service'
 import { Client, Request } from '@pepperi-addons/debug-server'
+import { ViewsService } from './views.service';
+import { UDCService } from './udc.service';
 
 export async function get_all_collections(client: Client, request: Request){
     validateRequest('GET', request)
-    const service = new UtilitiesService(client);
+    const service = new UDCService(client);
     return await service.getAllUDCCollections();
 }
 export async function get_collection_data(client: Client, request: Request){
@@ -11,7 +13,7 @@ export async function get_collection_data(client: Client, request: Request){
     if(!request?.query?.collectionName){
         throw new Error('collection name must be exist in request query, instead request query =  ' + request.query);
     }
-    const service = new UtilitiesService(client)
+    const service = new UDCService(client)
     return await service.getCollectionDataByName(request.query.collectionName)
 }
 function validateRequest(method: string, request: Request){
@@ -20,7 +22,7 @@ function validateRequest(method: string, request: Request){
     }
 }
 export async function views(client: Client, request: Request){
-    const service = new UtilitiesService(client)
+    const service = new ViewsService(client)
     if(request.method =='POST'){
         if(!request.body){
             throw new Error(`body must be exist when posting view, body: ${request.body}`)
