eb9939b42bdcc5a52675b383f89e1014b2a24be3
commit <>
2022-05-24 10:33:40 +0300

    pr change

--- server-side/metadata.ts
+++ server-side/metadata.ts
@@ -38,14 +38,3 @@ export const viewsTable: AddonDataScheme = {
         }
     }
 }
-
-export function isView(arg: any): arg is View {
-    return arg && arg.Key && typeof(arg.Key) === 'string' && arg.Name && typeof(arg.Name) === 'string' && arg.Resource && isResource(arg.Resource) &&
-     (!arg.Sorting || isSorting(arg.Sorting)) && arg.Description && typeof(arg.Description) === 'string'
-}
-function isResource(arg: any): arg is Resource{
-    return arg.AddonUUID && arg.Name && typeof(arg.AddonUUID) === 'string' && typeof(arg.Name) === 'string'
-}
-function isSorting(arg: any): arg is Sorting{
-    return arg.FieldKey && arg.Ascending && typeof(arg.FieldKey) === 'string' && typeof(arg.Ascending) === 'boolean'
-}
