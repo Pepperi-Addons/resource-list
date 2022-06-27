69de6c19588e67ba777fb88356a6c062330fb3cb
commit <>
2022-06-27 11:09:57 +0300

    pr change

---
+++ client-side/src/app/editors/editor-form.ts
@@ -0,0 +1,34 @@
+import { Editor, OpenMode, View } from "../../../../shared/entities";
+import { AbstractForm } from "./form";
+
+export class EditorForm extends AbstractForm{
+  content: Editor;
+  async initView(key: string){
+      if(key === "new"){
+        this.content = {
+          Key: "",
+          Name: "",
+          Description: "",
+          Resource: {
+            AddonUUID: "",
+            Name: ""
+          },
+          OpenMode: "same-page"
+        }
+      }
+      else{
+        this.content = (await this.service.getEditors(key))[0]
+      }
+  }
+  async update() {
+    this.content.Name = this.dataSource.Name
+    this.content.Description = this.dataSource.Description
+    this.content.Resource.Name = this.dataSource.Resource
+    this.content.OpenMode = this.dataSource.OpenMode
+    this.service.upsertEditor(this.content)
+  }
+  setOpenMode(openMode: OpenMode){
+    debugger
+    this.dataSource.OpenMode = openMode
+  }
+}
---
+++ client-side/src/app/editors/form.ts
@@ -0,0 +1,126 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { ViewsService } from '../services/views.service';
+import { UDCService } from '../services/udc-service';
+import { config } from '../addon.config';
+import { View, Editor } from '../../../../shared/entities';
+
+export abstract class AbstractForm{
+    dataSource: any
+    dataView: any = {
+        Type: "Form",
+        Fields: [],
+        Context: {
+          Name: "",
+          Profile: {},
+          ScreenSize: 'Tablet'
+        }
+    }
+    abstract content: View | Editor
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private translate: TranslateService,
+        private udcService: UDCService,
+        protected service: ViewsService,
+        ){ 
+          this.udcService.pluginUUID = config.AddonUUID
+        }
+    async init(){
+        await this.initView(this.route.snapshot.paramMap.get('key'))
+        this.initDataSource();
+        await this.initDataView()
+    }
+    abstract initView(key: string):Promise<any>;
+
+    initDataSource(){
+        this.dataSource = {
+            Name: this.content.Name,
+            Description: this.content.Description,
+            Resource: this.content.Resource.Name
+          }
+    }
+    async initDataView(){
+        this.dataView =  {
+          Type: "Form",
+          Fields: await this.getFields(),
+          Context: {
+            Name: "",
+            Profile: {},
+            ScreenSize: 'Tablet'
+          }
+        };
+      }
+    async getFields(){
+        return [
+            {
+            ReadOnly: false,
+            Title: this.translate.instant('Name'),
+            Type: 'TextBox',
+            FieldID: "Name",
+            Mandatory: false,
+            Layout: {
+                Origin: {
+                X: 0,
+                Y:0
+                },
+                Size: {
+                Width: 1,
+                Height: 0
+                }
+            }
+            },
+            {
+            ReadOnly: false,
+            Title: this.translate.instant('Description'),
+            Type: 'TextBox',
+            FieldID: "Description",
+            Mandatory: false,
+            Layout: {
+                Origin: {
+                X: 1,
+                Y: 0
+                },
+                Size: {
+                Width: 1,
+                Height: 0
+                }
+            }
+            },
+            {
+            ReadOnly: false,
+            Title: this.translate.instant('Resource'),
+            Type: 'ComboBox',
+            FieldID: "Resource",
+            Mandatory: false,
+            OptionalValues: await this.getResourcesNames(),
+            Layout: {
+                Origin: {
+                X: 0,
+                Y:1
+                },
+                Size: {
+                Width: 1,
+                Height: 0
+                }
+            }
+            }
+        ]
+    }
+    async getResourcesNames(){
+        const resources = await this.udcService.getCollections()
+        return resources.map(resource => {
+            return {'Key': resource.Name, 'Value': resource.Name}
+        })
+    }
+    getDataSource(){
+        return this.dataSource
+    }
+    getDataView(){
+        return this.dataView
+    }
+    abstract update()
+    getName(){
+        return this.content.Name
+    }
+}
