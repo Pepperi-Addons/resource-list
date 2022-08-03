87082160fa0c11d494c73b1e48e0b9dd9a7135b8
commit <>
2022-08-03 16:18:53 +0300

    add progress dots

--- client-side/src/app/services/editors.service.ts
+++ client-side/src/app/services/editors.service.ts
@@ -2,21 +2,25 @@ import { Injectable } from "@angular/core";
 import { config } from "../addon.config";
 import { UtilitiesService } from './utilities-service'
 import { IDataService } from '../metadata'
+import { PepHttpService } from "@pepperi-addons/ngx-lib";
 
 @Injectable({ providedIn: 'root' })
 export class EditorsService implements IDataService{
     pluginUUID;
     constructor(
         private utilitiesService: UtilitiesService,
+        private pepHttp: PepHttpService
     ){
     }
     async getItems(key: string = undefined, includeDeleted = false){
-        const deletedQuery = includeDeleted? `Hidden=true` : `Hidden=false`
-        const query: any = key? {where:'Key=' + '"' + key + '"' + " AND " + deletedQuery} : {where: deletedQuery}
-        query.include_deleted = includeDeleted
-        return await this.utilitiesService.papiClient.addons.api.uuid(config.AddonUUID).file('api').func('editors').get(query)
+        let url = `/addons/api/${config.AddonUUID}/api/editors`
+        let query = `?include_deleted=${includeDeleted}&where=Hidden=${includeDeleted}`
+        if(key){
+            query = query + ` AND Key='${key}'`
+        }
+        return await this.pepHttp.getPapiApiCall(url + query).toPromise()
     }
     async upsertItem(editor: any){
-        return await this.utilitiesService.papiClient.addons.api.uuid(config.AddonUUID).file('api').func('editors').post(undefined,editor)
+        return await this.pepHttp.postPapiApiCall(`/addons/api/${config.AddonUUID}/api/editors`, editor).toPromise()
     }
 }
