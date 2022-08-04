fcf0ee70768af16c376d446f6cc68712525bd99d
commit <>
2022-08-04 11:33:00 +0300

    bugfix update styleURLs

--- client-side/src/app/line-menu-tab/line-menu-tab.component.ts
+++ client-side/src/app/line-menu-tab/line-menu-tab.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-line-menu-tab',
   templateUrl: './line-menu-tab.component.html',
-  styleUrls: ['./line-menu-tab.component.css']
+  styleUrls: ['./line-menu-tab.component.scss']
 })
 export class LineMenuTabComponent implements OnInit {
 
