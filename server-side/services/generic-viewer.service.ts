f9ef53697d974f66dcc96377d94887e4f30f41c5
commit <>
2022-10-25 13:09:46 +0300

    get view/editor by key

--- server-side/services/generic-viewer.service.ts
+++ server-side/services/generic-viewer.service.ts
@@ -16,8 +16,8 @@ export class GenericViewerService  {
         const dataViewService = new DataViewsService(this.client)
         const editorService = new EditorsService(this.client)
         const dataViewKey = viewKey.replace(/-/g, '')
-        const [[view],[viewDataview], [lineMenuItems], [menuItems]] = await Promise.all([
-            viewService.getItems({where: `Key="${viewKey}"`}),
+        const [view,[viewDataview], [lineMenuItems], [menuItems]] = await Promise.all([
+            viewService.getItemByKey(viewKey),
             dataViewService.getDataView(`GV_${dataViewKey}_View`),
             dataViewService.getDataView(`RV_${dataViewKey}_LineMenu`),
             dataViewService.getDataView(`GV_${dataViewKey}_Menu`)
@@ -32,8 +32,8 @@ export class GenericViewerService  {
         }
         if(view.Editor){
             const editorDataViewKey = view.Editor.replace(/-/g, '')
-            const [[editor], [editorDataView]] = await Promise.all([
-                editorService.getItems({where: `Key = "${view.Editor}"`}),
+            const [editor, [editorDataView]] = await Promise.all([
+                editorService.getItemByKey(view.Editor),
                 dataViewService.getDataView(`GV_${editorDataViewKey}_Editor`)
 
             ])    
@@ -50,7 +50,7 @@ export class GenericViewerService  {
         if(!key){
             view = await viewsService.getDefaultView(resource!) as View;
         }else{
-            [view] = await viewsService.getItems({where: `Key="${key}"`}) as View[];
+            view = await viewsService.getItemByKey(key) as View
         }
         const dataViewKey = view.Key.replace(/-/g, '');
         const [dataview] = await dataViewService.getDataView(`GV_${dataViewKey}_View`) as GridDataView[]
