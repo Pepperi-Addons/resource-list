68b94c7c57b01eb002e1353eb04b0a0c83cc5fec
commit <>
2022-11-07 17:14:05 +0200

    bugfix

--- client-side/src/app/data-configuration-block/data-configuration-block.component.ts
+++ client-side/src/app/data-configuration-block/data-configuration-block.component.ts
@@ -24,6 +24,7 @@ export class DataConfigurationBlockComponent implements OnInit {
     dataView: any
     currentEditorKey: string
     editor: Editor
+    loadCompleted = false
     @Output() hostEvents: EventEmitter<any> = new EventEmitter<any>();
 
     constructor(private translate: TranslateService,
@@ -36,16 +37,19 @@ export class DataConfigurationBlockComponent implements OnInit {
     }
     ngOnChanges($event){
       this.loadBlock()
+
     }
     async loadBlock(){
       this.currentEditorKey = this.hostObject?.configuration?.currentEditorKey
       if(this.currentEditorKey != undefined){
         [this.editor] = await this.editorsService.getItems(this.currentEditorKey)
         const editorDataViews = await this.dataViewService.getDataViews(`GV_${this.currentEditorKey}_Editor`)
-        this.dataView = editorDataViews[0] 
+        this.dataView = editorDataViews[0]
+        this.loadCompleted = true 
 
       }else{
         this.dataView = {}
       }
+      
     }
 }
