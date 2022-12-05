b8b247ff86f5bc279c9565e7db05df617c3eac9e
commit <>
2022-12-05 16:43:23 +0200

    use the online service on settings

--- client-side/src/app/services/generic-resource-service.ts
+++ client-side/src/app/services/generic-resource-service.ts
@@ -14,11 +14,9 @@ export class GenericResourceService{
     pluginUUID;
     constructor(
         private addonService: PepAddonService,
-        private utilitiesService: UtilitiesService
+        private utilitiesService: UtilitiesService,
+        private pepHttp: PepHttpService
     ){
-    }
-    async getOfflineResources(): Promise<any[]>{
-        return await this.addonService.getAddonCPICall(config.AddonUUID,`${GENERIC_RESOURCE_OFFLINE_URL}/resources`) || []
     }
     async getResources(): Promise<any[]>{
         return await this.utilitiesService.papiClient.resources.resource('resources').get()
@@ -29,16 +27,16 @@ export class GenericResourceService{
             if(filterQuery){
                 query.where += ` AND ${filterQuery}`
             }
-           return await this.addonService.postAddonCPICall(config.AddonUUID, `${GENERIC_RESOURCE_OFFLINE_URL}/get_items/${resourceName}`, {query: query})
+            return await this.utilitiesService.papiClient.resources.resource(resourceName).get(query)
         }catch(e){
             return []
         }
     }
     async postItem(resourceName, item){
-        return await this.addonService.postAddonCPICall(config.AddonUUID, `${GENERIC_RESOURCE_OFFLINE_URL}/post_item/${resourceName}`, item)
+        return await this.utilitiesService.papiClient.resources.resource(resourceName).post(item)
     }
     async getResource(name: string){
-        return await this.addonService.getAddonCPICall(config.AddonUUID, `${GENERIC_RESOURCE_OFFLINE_URL}/${name}`) as AddonDataScheme
+        return await this.utilitiesService.papiClient.resources.resource('resources').key(name).get() as AddonDataScheme
     }
 
     async getResourceFields(resourceName: string): Promise<AddonDataScheme['Fields']>{
@@ -60,14 +58,16 @@ export class GenericResourceService{
         return  [...fields, ...defaultCollectionFields ]
     }
     async getGenericView(viewKey: string){
-        return await this.addonService.getAddonCPICall(config.AddonUUID, `${GENERIC_VIEWS_RESOURCE}/generic_view?Key=${viewKey}`)
+        let url = `/addons/api/${config.AddonUUID}/api/get_generic_view?Key=${viewKey}`
+        return await this.pepHttp.getPapiApiCall(url).toPromise()
     }
     async getSelectionList(viewKey?: string, resource?: string){
+        let url = `/addons/api/${config.AddonUUID}/api/get_selection_list?`
         if(viewKey){
-            return await this.addonService.getAddonCPICall(config.AddonUUID, `${GENERIC_VIEWS_RESOURCE}/selection_list?Key=${viewKey}`)
+            return await this.pepHttp.getPapiApiCall(url + `Key=${viewKey}`).toPromise()
         }
         else if(resource){
-            return await this.addonService.getAddonCPICall(config.AddonUUID, `${GENERIC_VIEWS_RESOURCE}/selection_list?Resource=${resource}`)
+            return await this.pepHttp.getPapiApiCall(url + `Resource=${resource}`).toPromise()
         }
         else{
             return undefined
