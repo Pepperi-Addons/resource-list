420d458e33d8e1cdcaa970ce92baeb696c85a74b
commit <michael.a@pepperi.com>
2023-03-15 12:48:42 +0200

    remove debugging code

--- client-side/src/app/resource-list-0.9/helpers/RL-data-source.ts
+++ client-side/src/app/resource-list-0.9/helpers/RL-data-source.ts
@@ -90,19 +90,6 @@ export class RLDataSource implements IRLDataSource{
     }
     
     async init(params: IPepGenericListParams): Promise<IPepGenericListInitData>{
-        debugger
-        if(params.filters){
-            const jsonFilters = NgXToJSONFilterAdapter.adapt(params.filters, {
-                Name: {
-                    Type: 'String'
-                },
-                age: {
-                    Type: 'DateTime'
-                }
-            })
-            const ngxFilters = JSONToNGXFilterAdapter.adapt(jsonFilters)
-            debugger
-        }
         //if the list is just cloned there is no need to emit an event because the first init happen because the data source was cloned! and not because an event
         if(!this.isCloned){
             const paramsAdapter = new GLParamsAdapter(params)
