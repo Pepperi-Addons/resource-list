81e48d9943a8b37a0ee2108f3530c8a4e5471632
michael.a@pepperi.com <michael.a@pepperi.com>
2023-03-21 10:36:04 +0200

    changes on load list must contain list key

--- cpi-side/events/contorllers/load-list.controller.ts
+++ cpi-side/events/contorllers/load-list.controller.ts
@@ -5,8 +5,8 @@ export class LoadListController{
     static async loadList(state: Partial<ListState>, changes: ListState, list?: List){
         try{
             //if we don't get a key from the list and not from the changes, there could not be any load list
-            if(!list?.Key && (!changes || !changes.ListKey)){
-                throw Error(`list or changes are required,  and needs to contain ListKey`)
+            if(!changes || !changes.ListKey){
+                throw Error(` changes are required  and needs to contain ListKey`)
             }
             return await new LoadListEventService().execute(state, changes, list)
         }catch(err){
