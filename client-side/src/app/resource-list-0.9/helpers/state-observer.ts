0253eb8b694d889c60d97115bf356f8df38f0db0
michaelatias <87411559+michaelatias@users.noreply.github.com>
2023-06-22 13:14:12 +0300

    add support of select all (#239)

--- client-side/src/app/resource-list-0.9/helpers/state-observer.ts
+++ client-side/src/app/resource-list-0.9/helpers/state-observer.ts
@@ -11,12 +11,14 @@ export class StateObserver{
     private $pageType: ReplaySubject<IPepGenericListPager['type']> = new ReplaySubject()
     private $sorting: ReplaySubject<IPepListSortingChangeEvent> = new ReplaySubject()
     private $selectedViewKey: ReplaySubject<string>  = new ReplaySubject()
+    private $selectAll: ReplaySubject<boolean>  = new ReplaySubject()
 
     notifyObservers(state: Partial<ListState>){
         this.$searchString.next(state.SearchString)
         this.$pageIndex.next(state.PageIndex)
         this.$pageSize.next(state.PageSize)
         this.$pageType.next(state.PageType?.toLowerCase() as IPepGenericListPager['type'])
+        this.$selectAll.next(state.ItemSelection?.SelectAll || false)
         if(state.Sorting){
             this.$sorting.next({
                 isAsc: state.Sorting.Ascending,
@@ -50,6 +52,10 @@ export class StateObserver{
     }
     onPageTypeChanged(cb: (type: IPepGenericListPager['type']) => void): StateObserver{
         this.$pageType.subscribe(cb)
+        return this
+    }
+    onSelectAllChanged(cb: (isAllSelected: boolean) => void): StateObserver{
+        this.$selectAll.subscribe(cb)
         return this
     }
 }
