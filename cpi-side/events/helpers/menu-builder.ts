2a1929bf9ad704491b46eed8f61af919554799e4
michael.a@pepperi.com <michael.a@pepperi.com>
2023-06-29 09:36:46 +0300

    pr changes

--- cpi-side/events/helpers/menu-builder.ts
+++ cpi-side/events/helpers/menu-builder.ts
@@ -29,12 +29,14 @@ export class MenuBuilder{
         }))
     }
     async callDrawBlockFunction(drawURL: drawFunctionObject, state: Partial<ListState> | undefined, changes: Partial<ListState>): Promise<MenuBlock[] | undefined>{
-        const result =  await pepperi.addons.api.uuid(drawURL.AddonUUID).post({
+        let result =  await pepperi.addons.api.uuid(drawURL.AddonUUID).post({
             url: drawURL.DrawURL,
             body: { Changes: changes, State: state }
         })
+        //in case of an error return an empty array of blocks
         if(!result){
-            throw Error(`can't draw  menu blocks for adddon uuid ${drawURL.AddonUUID} and url ${drawURL.DrawURL}`)
+            console.error(`can't draw  menu blocks for adddon uuid ${drawURL.AddonUUID} and url ${drawURL.DrawURL}`)
+            result = {Result: []}
         }
         return result.Result as (MenuBlock[] | undefined)
     }
